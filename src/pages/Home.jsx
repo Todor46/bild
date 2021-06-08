1194e6bd2c57e0e2c768e38214f97da6139cc88d
Todor Konjevic <todor.konjevic@gmail.com>
2021-06-08 19:02:24 +0200

    Add hero section to home.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import "../styles/home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="text-center hero">
+      <img src="/images/slider.png" alt="hero" className="w-full" />
+      <div className="max-w-screen-md  mx-auto text-[#aeadad] mt-[50px] font-medium">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu
+        eratiuy lacus, vel congue mauris. Fusce velitaria justo, faucibus eu.
+      </div>
+      <button className="uppercase bg-green-primary font-medium text-white px-4 py-2 mt-5 text-sm">
+        Browse portfolio
+      </button>
+    </section>
+  );
 };
 
 export default Home;
