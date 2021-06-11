5a394d18507c383784ec91c2061708d4304a8491
Todor Konjevic <todor.konjevic@gmail.com>
2021-06-11 12:57:51 +0200

    Add project type property to generate.js.

--- generate.js
+++ generate.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 const faker = require("faker");
 
 module.exports = function () {
+  const types = ["print", "photography", "web", "applications"];
   return {
     projects: _.times(50, (n) => {
       return {
@@ -9,6 +10,7 @@ module.exports = function () {
         title: faker.commerce.productName(),
         description: faker.lorem.words(_.random(10, 25)),
         image: `${faker.image.business()}?random=${_.random(0, 1000)}`,
+        type: types[_.random(0, types.length - 1)],
       };
     }),
   };
