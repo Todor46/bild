398a5d78d11ec6481e4b9d8cb911832f1566fc52
Todor Konjevic <todor.konjevic@gmail.com>
2021-06-14 17:10:24 +0200

    Style the NotFound page and add go back button.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
-const NotFound = () => {
-  return "Requested page not found";
+const NotFound = ({ history }) => {
+  return (
+    <div className="max-w-screen-lg mx-auto px-4 lg:px-0 flex flex-col items-center justify-center py-40">
+      <h2 className="text-2xl font-semibold text-gray-600 text-center">
+        The requested page does not exist.
+      </h2>
+      <button
+        className="hover:underline mt-2 cursor-pointer text-green-500"
+        onClick={() => history.goBack()}
+      >
+        Go back
+      </button>
+    </div>
+  );
 };
 
 export default NotFound;
